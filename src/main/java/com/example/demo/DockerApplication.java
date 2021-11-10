47139531f563aa651ee69983c91dc228c8349a60
solutionman <solutionman@inbox.ru>
2021-11-10 12:31:02 +0700

    another way save to DB after app start

--- src/main/java/com/example/demo/DockerApplication.java
+++ src/main/java/com/example/demo/DockerApplication.java
@@ -2,17 +2,23 @@ package com.example.demo;
 
 import com.example.demo.entity.AppUser;
 import com.example.demo.entity.Hero;
+import com.example.demo.entity.Role;
 import com.example.demo.repository.HeroRepository;
 import com.example.demo.repository.AppUserRepository;
+import com.example.demo.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -43,7 +49,7 @@ public class DockerApplication {
         List<Hero> allHeroes = heroRepository.findAll();
         logger.info("Heroes: {}", allHeroes.size());
 
-        if (userRepository.findAll().isEmpty()) {
+        if (userRepository.findAppUserByUserName("User") != null && userRepository.findAppUserByUserName("Admin") != null) {
             AppUser user = new AppUser();
             user.setUserName("User");
             user.setPassword(passwordEncoder.encode("12345678"));
@@ -55,7 +61,24 @@ public class DockerApplication {
             userAdmin.setPassword(passwordEncoder.encode("87654321"));
             userRepository.save(userAdmin);
             logger.info("User: {} Pass: {}", userAdmin.getUserName(), userAdmin.getPassword());
+        } else {
+            logger.info("User or Admin already exists in database");
         }
+    }
 
+    // Another way to add something to DB after start application
+    @Bean
+    CommandLineRunner run(UserService userService) {
+        return args -> {
+            userService.saveRole(new Role(null, "ROLE_USER"));
+            userService.saveRole(new Role(null, "ROLE_MANAGER"));
+            userService.saveRole(new Role(null, "ROLE_ADMIN"));
+
+            userService.saveUser(new AppUser(null, "SuperUser", passwordEncoder.encode("12345678"), new HashSet<>()));
+            userService.addRoleToAppUser("SuperUser", "ROLE_USER");
+
+            userService.saveUser(new AppUser(null, "SuperAdmin", passwordEncoder.encode("87654321"), new HashSet<>()));
+            userService.addRoleToAppUser("SuperAdmin", "ROLE_ADMIN");
+        };
     }
 }
--- src/main/java/com/example/demo/entity/Role.java
+++ src/main/java/com/example/demo/entity/Role.java
@@ -1,6 +1,8 @@
 package com.example.demo.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
@@ -10,6 +12,8 @@ import javax.persistence.Id;
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Role {
     @Id
     @GeneratedValue
