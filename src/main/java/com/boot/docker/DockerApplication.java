00fdca90b6198025fed9d4c96652ce9a1e2eb48e
solutionman <solutionman@inbox.ru>
2022-07-30 00:39:42 +0700

    sql commented

--- src/main/java/com/boot/docker/DockerApplication.java
+++ src/main/java/com/boot/docker/DockerApplication.java
@@ -39,46 +39,47 @@ public class DockerApplication {
         System.out.println("hello");
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void runAfterStartup() {
-        Hero hero = new Hero();
-        hero.setHeroName("Terminator");
-        hero.setActorName("Arnold");
-        this.heroRepository.save(hero);
-        List<Hero> allHeroes = heroRepository.findAll();
-        logger.info("Heroes: {}", allHeroes.size());
+//    @EventListener(ApplicationReadyEvent.class)
+//    public void runAfterStartup() {
+//        Hero hero = new Hero();
+//        hero.setHeroName("Terminator");
+//        hero.setActorName("Arnold");
+//        this.heroRepository.save(hero);
+//        List<Hero> allHeroes = heroRepository.findAll();
+//        logger.info("Heroes: {}", allHeroes.size());
+//
+//        if (userRepository.findAppUserByUserName("User") != null && userRepository.findAppUserByUserName("Admin") != null) {
+//            AppUser user = new AppUser();
+//            user.setUserName("User");
+//            user.setPassword(passwordEncoder.encode("12345678"));
+//            userRepository.save(user);
+//            logger.info("User: {} Pass: {}", user.getUserName(), user.getPassword());
+//
+//            AppUser userAdmin = new AppUser();
+//            userAdmin.setUserName("Admin");
+//            userAdmin.setPassword(passwordEncoder.encode("87654321"));
+//            userRepository.save(userAdmin);
+//            logger.info("User: {} Pass: {}", userAdmin.getUserName(), userAdmin.getPassword());
+//        } else {
+//            logger.info("User or Admin already exists in database");
+//        }
+//    }
 
-        if (userRepository.findAppUserByUserName("User") != null && userRepository.findAppUserByUserName("Admin") != null) {
-            AppUser user = new AppUser();
-            user.setUserName("User");
-            user.setPassword(passwordEncoder.encode("12345678"));
-            userRepository.save(user);
-            logger.info("User: {} Pass: {}", user.getUserName(), user.getPassword());
-
-            AppUser userAdmin = new AppUser();
-            userAdmin.setUserName("Admin");
-            userAdmin.setPassword(passwordEncoder.encode("87654321"));
-            userRepository.save(userAdmin);
-            logger.info("User: {} Pass: {}", userAdmin.getUserName(), userAdmin.getPassword());
-        } else {
-            logger.info("User or Admin already exists in database");
-        }
-    }
-
+    // !!!!  Uncomment this code to add users when running app first time !!!!
     // Another way to add something to DB after start application
-    @Bean
-    CommandLineRunner run(UserService userService) {
-
-        return args -> {
-            userService.saveRole(new Role(null, "ROLE_USER"));
-            userService.saveRole(new Role(null, "ROLE_MANAGER"));
-            userService.saveRole(new Role(null, "ROLE_ADMIN"));
-
-            userService.saveUser(new AppUser(null, "SuperUser", "12345678", new HashSet<>()));
-            userService.addRoleToAppUser("SuperUser", "ROLE_USER");
-
-            userService.saveUser(new AppUser(null, "SuperAdmin", "87654321", new HashSet<>()));
-            userService.addRoleToAppUser("SuperAdmin", "ROLE_ADMIN");
-        };
-    }
+//    @Bean
+//    CommandLineRunner run(UserService userService) {
+//
+//        return args -> {
+//            userService.saveRole(new Role(null, "ROLE_USER"));
+//            userService.saveRole(new Role(null, "ROLE_MANAGER"));
+//            userService.saveRole(new Role(null, "ROLE_ADMIN"));
+//
+//            userService.saveUser(new AppUser(null, "SuperUser", "12345678", new HashSet<>()));
+//            userService.addRoleToAppUser("SuperUser", "ROLE_USER");
+//
+//            userService.saveUser(new AppUser(null, "SuperAdmin", "87654321", new HashSet<>()));
+//            userService.addRoleToAppUser("SuperAdmin", "ROLE_ADMIN");
+//        };
+//    }
 }
